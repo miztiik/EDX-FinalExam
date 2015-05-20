2304316da4ccc22609cf59133c00b1df1ca645d4
mystique <13oct08@quantumfoam.uni.me>
2015-05-21 01:38:49 +0530

    Finished this excercise, lot of lessons learn. - Cross validation for CP - Sparsity meaning - AUC meaning - how to write expand.grid etc Have tried to include as much explanation as possible for me understand in the future

--- predEbaySell.R
+++ predEbaySell.R
@@ -72,13 +72,82 @@ require("caret")
 require("e1071")
 
 rpartControl = trainControl( method ="cv" , number = 10)
-rpartGrid = expand.grid( .cp = seq(0.01,0.5,0.01))
+rpartGrid = expand.grid( .cp = seq(0.001,0.05,0.00098))
 
 print("= 10 Fold Cross Validation begins")
 tr = train(sold ~ biddable + startprice + condition + heel + style + color + material , data = dfTrain, method = "rpart", trControl = rpartControl, tuneGrid = rpartGrid)
 
-print("= 10 Fold Cross Validation ends & plotting the best model")
+print("### 10 Fold Cross Validation ends & plotting the best model ###")
 
 prp(tr$finalModel)
 
+#Building a model with cp "0.005" found after CV
+m2 = rpart(sold ~ biddable + startprice + condition + heel + style + color + material , data = dfTrain, method ="class" , cp = 0.005)
+prp(m2)
 
+#Building a corpus to peform text analytics to see if the description of shoes improves the prediction
+require("tm")
+require("SnowballC")
+require("ggvis")
+
+corpus = Corpus(VectorSource(df$description))
+corpus = tm_map(corpus, tolower)
+corpus = tm_map(corpus, PlainTextDocument)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+corpus = tm_map(corpus, stemDocument)
+dtm = DocumentTermMatrix(corpus)
+
+#Total number of tersm in the document
+dtm
+
+#Remove all terms that don't appear in at least 10% of documents,
+#i,e Limit dtm to contain terms appearing in atleast 10% of document - then set sparse to 0.9 i,e (100 -10)/100
+#Sparse value in "removeSparseTerms" = remove all terms who are NOT appearing in "X" of the documents, i,e they are (100-X%)/100 empty/sparse
+
+spdtm = removeSparseTerms(dtm, 0.9)
+
+#Creating a data frame from the sparsed Cropus
+descriptionText = as.data.frame(as.matrix(spdtm))
+
+#Find the word stem that shows up most frequently across all the documents
+which.max(colSums(descriptionText))
+
+#Add a prefix "D" for description in the column names to make it R-Friendly
+names(descriptionText) = paste0("D", names(descriptionText))
+
+#Adding the other variables to this df
+descriptionText$sold=df$sold
+descriptionText$biddable=df$biddable
+descriptionText$startprice=df$startprice
+descriptionText$condition=df$condition
+descriptionText$heel=df$heel
+descriptionText$style=df$style
+descriptionText$color=df$color
+descriptionText$material=df$material
+
+trainText = subset(descriptionText,spl==TRUE)
+testText = subset(descriptionText,spl==FALSE)
+
+#Logistic Regression using text analytics
+
+m3 = glm(sold ~ . , data = trainText, family = "binomial")
+summary(m3)
+
+#Predict using the new model with description field
+#Predicting within the Train set
+p3Train = predict(m3,type="response")
+#Calculating ROCR for Train set
+p3TrainROCR = prediction(p3Train,trainText$sold)
+p3TrainAUC = as.numeric(performance(p3TrainROCR,"auc")@y.values)
+p3TrainAUC
+
+#Predicting the test set
+p3 = predict(m3,newdata=testText,type="response")
+#Calculating ROCR for test set
+p3ROCR = prediction(p3,testText$sold)
+p3AUC = as.numeric(performance(p3ROCR,"auc")@y.values)
+p3AUC
+
+#Even with the additional variables in the model with "descriptions text" the AUC didn't improve compared to the previous glm model "m1" hence it is over fitted.
+#Removing variables might improve this model
