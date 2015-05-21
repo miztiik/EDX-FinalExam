87c58c8f2b5b91d13c8e0174c56760dd811a7214
mystique <13oct08@quantumfoam.uni.me>
2015-05-21 13:59:43 +0530

    Normalized the data and all set for K-Means Cluster. Next commit should have the code for KMeans

--- clustHubway.R
+++ clustHubway.R
@@ -1,4 +1,4 @@
-## EDX Final Exam
+# EDX Final Exam
 ## Third Exam - Clustering - Segmentation - Classification
 ## Hubway - Bicycle Renting company rents out its bike for a membership fee. Unlimited number of fides less 30 minutes are free.
 ## Longer rides will cost a "overtime" fees.
@@ -18,14 +18,46 @@ readData <- function() {
   ## Importing & Preparing the data ( using readr package faster than read.csv)
   df = read_csv("./input/FinalExam_HubwayTrips.csv")
 
+  df1 = df
 
   ## Removing columns which are not necessary for the predictions
 
   ##Changing the Character columns into Factors
+  df$Morning = as.factor(df$Morning)
+  df$Afternoon = as.factor(df$Afternoon)
+  df$Evening = as.factor(df$Evening)
+  df$Weekday = as.factor(df$Weekday)
+  df$Male = as.factor(df$Male)
 
   ##Combining the dataset to make them ready for text analysis
   assign("df", df, envir)
+  assign("df1", df1, envir)
 
   print("## Finished importing & preparing the data ##")
 }
 readData()
+
+#Average duration over the weekday
+tapply(df$Duration,df$Weekday,mean)
+
+#Proportion of Male among bikers
+mean(df$Male==1)
+
+#Normalizing the data to prepare them for clustering
+
+require("caret")
+preProc = preProcess(df1)
+#str(preProc)
+df1Norm = predict(preProc,df1)
+str(df1Norm)
+
+#Max duration after normalization
+max(df1Norm$Duration)
+
+#Lets just "attempt" a hierarchial cluster even though it is not useful. "Just too  many of them!!"
+#dist = dist(df1Norm, method = "euclidean" )
+#clus = hclust(dist, method = "ward" )
+#plot(clus)
+
+#Since Hierachial wont work, lets start building K-Means Cluster
+set.seed(5000)
